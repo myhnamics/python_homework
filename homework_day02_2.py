ae26f38c974a787289eb1badb3218cee73bcff9f
kimhw <rlagussdn2023@naver.com>
2023-01-13 16:21:50 +0900

    homework_day02_2

---
+++ homework_day02_2.py
@@ -0,0 +1,12 @@
+green = True
+small = True
+if green:
+    if small:
+        print('pea')
+    else:
+        print('watermelon')
+else:
+    if small:
+        print('cherry')
+    else:
+        print('pumkin')
