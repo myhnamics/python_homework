f79e05a02e16827135a8ac608ae4f731cfd99300
kimhw <rlagussdn2023@naver.com>
2023-02-07 19:47:24 +0900

    homework_day17_ch07_1_input

--- homework_day17_ch07.py
+++ homework_day17_ch07.py
@@ -29,7 +29,7 @@ def is_queue_empty():
 
 def de_queue():
     global size,queue,front,rear
-    if(is_queue_empty()):
+    if is_queue_empty():
         "큐가 비엇어"
         return
     front += 1
@@ -65,6 +65,31 @@ if __name__ == "__main__":
         print(de_queue(),'님 식당 들어감')
 
     print('식당 영업 종료!')
+
+    # 입력방식
+    if __name__ == "__main__":
+        select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
+
+        while select != 'X' and select != 'x':
+            if select == 'I' or select == 'i':
+                data = input("입력할 데이터 ==> ")
+                en_queue(data)
+                print("큐 상태 : ", queue)
+            elif select == 'E' or select == 'e':
+                data = de_queue()
+                print("추출된 데이터 ==> ", data)
+                print("큐 상태 : ", queue)
+            elif select == 'V' or select == 'v':
+                data = peek()
+                print("확인된 데이터 ==> ", data)
+                print("큐 상태 : ", queue)
+            else:
+                print("입력이 잘못됨")
+
+            select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
+
+        print("프로그램 종료!")
+
 
 
 
