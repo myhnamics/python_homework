a88973a9be6078ea761916a7a319adfe754acecf
kimhw <rlagussdn2023@naver.com>
2023-02-07 20:37:05 +0900

    homework_day17_ch07_2

--- homework_day17_ch07.py
+++ homework_day17_ch07.py
@@ -1,96 +1,98 @@
 def is_queue_full():
     global size, queue, front, rear
-    if rear != size-1:
-        return False
-    elif rear == size-1 and front == -1:
+    if (rear + 1) % size == front:
         return True
     else:
-        for i in range(front+1,size):
-            queue[i-1] = queue[i]
-            queue[i] = None
-        front -= 1
-        rear -= 1
         return False
+
 
 def en_queue(data):
-    global size,queue,front,rear
-    if(is_queue_full()):
+    global size, queue, front, rear
+    if (is_queue_full()):
         print("큐가 꽉참")
         return
-    rear += 1
+    rear = (rear + 1) % size
     queue[rear] = data
 
+
 def is_queue_empty():
-    global size,queue,front,rear
-    if(front == rear):
+    global size, queue, front, rear
+    if front == rear:
         return True
     else:
         return False
 
+
 def de_queue():
-    global size,queue,front,rear
-    if is_queue_empty():
+    global size, queue, front, rear
+    if (is_queue_empty()):
         "큐가 비엇어"
         return
-    front += 1
+    front = (front + 1) % size
     data = queue[front]
     queue[front] = None
-
-    for i in range(front+1,rear+1):
-        queue[i-1] = queue[i]
-        queue[i] = None
-    front = -1
-    rear -= 1
     return data
 
+
 def peek():
-    global size,queue,front,rear
+    global size, queue, front, rear
     if is_queue_empty():
         print("큐가 비어잇어")
         return None
-    return queue[front+1]
+    return queue[(front + 1) % size]
 
-size = 5
-queue = [None for _ in range(size)]
-front = rear = -1
+def waiting_time():
+    global size, queue, front, rear
+    sum_time = 0
+    for i in range(len(queue)):
+        if queue[i] == None:
+            continue
+        else:
+            sum_time += queue[i][1]
+    return sum_time
 
 if __name__ == "__main__":
-    en_queue('정국')
-    en_queue('뷔')
-    en_queue('지민')
-    en_queue('진')
-    en_queue('슈가')
+    select = input("사용(1)/고장(2)/환불(3)/기타(4)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
+    front = rear = 0
+    size = 10
+    queue = [None for _ in range(0, size)]
+    while (select != 'X' and select != 'x'):
+        if select == '1':
+            data = ('사용', 9)
+            en_queue(data)
+            print("큐 상태 : ", queue)
+            print("front : ", front, ", rear : ", rear)
+        elif select == '2':
+            data = ('고장',3)
+            en_queue(data)
+            print("큐 상태 : ", queue)
+            print("front : ", front, ", rear : ", rear)
+        elif select == '3':
+            data = ('환불', 4)
+            en_queue(data)
+            print("큐 상태 : ", queue)
+            print("front : ", front, ", rear : ", rear)
+        elif select == '4':
+            data = ('기타',1)
+            en_queue(data)
+            print("큐 상태 : ", queue)
+            print("front : ", front, ", rear : ", rear)
+        elif select == 'E' or select == 'e':
+            data = de_queue()
+            print("추출된 데이터 ==> ", data)
+            print("큐 상태 : ", queue)
+            print("front : ", front, ", rear : ", rear)
+        elif select == 'V' or select == 'v':
+            data = peek()
+            print("확인된 데이터 ==> ", data)
+            print("큐 상태 : ", queue)
+            print("front : ", front, ", rear : ", rear)
+            print(waiting_time())
+        else:
+            print("입력이 잘못됨")
 
-    for _ in range(rear+1):
-        print(de_queue(),'님 식당 들어감')
-
-    print('식당 영업 종료!')
-
-    # 입력방식
-    if __name__ == "__main__":
         select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
 
-        while select != 'X' and select != 'x':
-            if select == 'I' or select == 'i':
-                data = input("입력할 데이터 ==> ")
-                en_queue(data)
-                print("큐 상태 : ", queue)
-            elif select == 'E' or select == 'e':
-                data = de_queue()
-                print("추출된 데이터 ==> ", data)
-                print("큐 상태 : ", queue)
-            elif select == 'V' or select == 'v':
-                data = peek()
-                print("확인된 데이터 ==> ", data)
-                print("큐 상태 : ", queue)
-            else:
-                print("입력이 잘못됨")
-
-            select = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
-
-        print("프로그램 종료!")
-
-
-
+    print("프로그램 종료!")
 
 
